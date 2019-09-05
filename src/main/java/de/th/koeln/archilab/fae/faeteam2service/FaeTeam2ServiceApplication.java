64719259167c9b75902469c26ca0bd85163c4db0
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-09-05 15:46:51 +0200

    adds spring boot project template

---
+++ src/main/java/de/th/koeln/archilab/fae/faeteam2service/FaeTeam2ServiceApplication.java
@@ -0,0 +1,13 @@
+package de.th.koeln.archilab.fae.faeteam2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaeTeam2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaeTeam2ServiceApplication.class, args);
+	}
+
+}
