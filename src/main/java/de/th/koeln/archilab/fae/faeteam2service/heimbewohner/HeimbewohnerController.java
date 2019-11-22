72d3e0a8f70a9e699ab203929414c226cae12b75
RedAvenge <thepowerredstone@gmail.com>
2019-11-22 10:50:22 +0100

    Remove old code

--- src/main/java/de/th/koeln/archilab/fae/faeteam2service/heimbewohner/HeimbewohnerController.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam2service/heimbewohner/HeimbewohnerController.java
@@ -1,11 +1,10 @@
 package de.th.koeln.archilab.fae.faeteam2service.heimbewohner;
 
-import de.th.koeln.archilab.fae.faeteam2service.verantwortlicher.Verantwortlicher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HeimbewohnerController {
@@ -22,12 +21,4 @@ public class HeimbewohnerController {
         return heimbewohnerRepository.save(heimbewohner);
     }
 
-    @GetMapping("/heimbewohner/{id}/verantwortlicher")
-    public Iterable<Verantwortlicher> heimbewohnerVerantwortliche(@PathVariable Integer id) {
-        Optional<Heimbewohner> heimbewohner = heimbewohnerRepository.findById(id);
-        if (heimbewohner.isPresent())
-            return heimbewohner.get().getVerantwortliche();
-        else
-            return new ArrayList<>();
-    }
 }
