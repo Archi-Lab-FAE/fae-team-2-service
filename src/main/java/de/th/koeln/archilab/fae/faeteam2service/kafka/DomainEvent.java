d4eea3e05eb1bd14f1ae6bc4bf8d54c951f0f7e6
= <=>
2019-12-28 14:32:51 +0100

    Add Kafka-Support

---
+++ src/main/java/de/th/koeln/archilab/fae/faeteam2service/kafka/DomainEvent.java
@@ -0,0 +1,18 @@
+package de.th.koeln.archilab.fae.faeteam2service.kafka;
+
+import java.util.UUID;
+
+public abstract class DomainEvent {
+
+    private final String eventID;
+
+    public DomainEvent() {
+        this.eventID = UUID.randomUUID().toString();
+    }
+
+    public abstract String getEventType();
+
+    public String getEventID() {
+        return this.eventID;
+    }
+}
