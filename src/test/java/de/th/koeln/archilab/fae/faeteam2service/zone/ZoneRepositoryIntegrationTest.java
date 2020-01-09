7a4006278db5d8cb86ce3e30903a84154a13a477
RedAvenge <thepowerredstone@gmail.com>
2020-01-09 16:14:12 +0100

    Fix kafka error on tests

--- src/test/java/de/th/koeln/archilab/fae/faeteam2service/zone/ZoneRepositoryIntegrationTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam2service/zone/ZoneRepositoryIntegrationTest.java
@@ -7,12 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EmbeddedKafka
 @AutoConfigureTestEntityManager
 @Transactional
 public class ZoneRepositoryIntegrationTest {
