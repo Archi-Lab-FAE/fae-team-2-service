4b47860a83f06ae2a49b71e66e3ecf36a83b664b
pschm <philipp.schmeier@gmail.com>
2020-01-10 22:58:33 +0100

    Fix embedded Kafka v2

--- src/test/java/de/th/koeln/archilab/fae/faeteam2service/positionssender/PositionssenderTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam2service/positionssender/PositionssenderTest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,6 +27,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @EmbeddedKafka
 @AutoConfigureMockMvc
+@DirtiesContext
+@TestPropertySource(properties = { "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}" })
 public class PositionssenderTest {
 
     @Autowired
