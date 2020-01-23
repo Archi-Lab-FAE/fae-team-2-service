8a65261918132fc57cad00e71ba5e436193e2ceb
RedAvenge <thepowerredstone@gmail.com>
2020-01-23 18:32:03 +0100

    Fix swagger ui redirect

--- src/main/java/de/th/koeln/archilab/fae/faeteam2service/configuration/HomeController.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam2service/configuration/HomeController.java
@@ -2,7 +2,6 @@ package de.th.koeln.archilab.fae.faeteam2service.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -17,12 +16,9 @@ public class HomeController {
 
     private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
-    @Value("${gateway-path:}")
-    private String gatewayPath;
-
     @GetMapping(value = "/")
     public String index() {
         log.info("Redirected to swagger-ui.html");
-        return "redirect:" + gatewayPath + "/swagger-ui.html";
+        return "redirect:/swagger-ui.html";
     }
 }
