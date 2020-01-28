32e8e4779a05e01076f6510dedf5a737d269d0da
sheila62 <sheila.kolodziej@gmx.de>
2020-01-28 10:33:00 +0100

    Added Business Logik Test "inZone" und "isInnerhalbRadius"

--- src/test/java/de/th/koeln/archilab/fae/faeteam2service/positionssender/PositionssenderTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam2service/positionssender/PositionssenderTest.java
@@ -1,10 +1,18 @@
 package de.th.koeln.archilab.fae.faeteam2service.positionssender;
 
+import de.th.koeln.archilab.fae.faeteam2service.demenziell_erkrankter.DemenziellErkrankter;
+import de.th.koeln.archilab.fae.faeteam2service.position.Position;
+import de.th.koeln.archilab.fae.faeteam2service.position.PositionDTO;
+import de.th.koeln.archilab.fae.faeteam2service.zone.Zone;
+import de.th.koeln.archilab.fae.faeteam2service.zone.ZoneDTO;
+import de.th.koeln.archilab.fae.faeteam2service.zone.ZonenTyp;
+import org.junit.Assert;
 import org.junit.Test;
 import org.threeten.bp.OffsetDateTime;
 import org.threeten.bp.ZoneOffset;
 
-import java.util.Random;
+import java.util.*;
+
 
 import de.th.koeln.archilab.fae.faeteam2service.demenziell_erkrankter.DemenziellErkrankter;
 import de.th.koeln.archilab.fae.faeteam2service.position.Position;
@@ -76,6 +84,79 @@ public class PositionssenderTest {
         assertEquals(1f, positionssender.getBatterieStatus().floatValue(),0.0002);
         assertEquals(letztesSignal, positionssender.getLetztesSignal());
         assertEquals(position.getBreitengrad(), positionssender.getPosition().getBreitengrad());
+
+    }
+    @Test
+    public void positionssenderIstInZoneKorrektInitialisiert() throws Exception {
+        Position position = new Position(2.0,3.0);
+        Position position2 =   new Position(2.4,3.3);
+        Set<Position> positionsset = new HashSet<>();
+        positionsset.add(position);
+        positionsset.add(position2);
+        Zone zone = new Zone(2f, ZonenTyp.GEWOHNT, positionsset);
+
+        Positionssender positionssender = new Positionssender();
+        String letztesSignal = getRandomDate().toString();
+        Position positionPositionssender = new Position();
+        positionPositionssender.setBreitengrad(3.1);
+        positionPositionssender.setLaengengrad(2.1);
+        positionssender.setLetztesSignal(letztesSignal);
+        positionssender.setPosition(positionPositionssender);
+        positionssender.setPositionssenderId("123456789");
+
+        Boolean posSenderInZone =positionPositionssender.inZone(zone);
+        Assert.assertTrue(posSenderInZone);
+    }
+    @Test
+    public void positionssenderIstInZoneFalschInitialisiert() throws Exception {
+        Boolean exception = false;
+        Position position = new Position(43.0,43.6);
+        Set<Position> positionsset = new HashSet<>();
+        positionsset.add(position);
+        Zone zone = new Zone(2f, ZonenTyp.GEWOHNT, positionsset);
+
+        Positionssender positionssender = new Positionssender();
+        String letztesSignal = getRandomDate().toString();
+        Position positionPositionssender = new Position();
+        positionPositionssender.setBreitengrad(43.6);
+        positionPositionssender.setLaengengrad(43.0);
+        positionssender.setLetztesSignal(letztesSignal);
+        positionssender.setPosition(positionPositionssender);
+        positionssender.setPositionssenderId("1234");
+
+        try {
+            Boolean posSenderInZone = positionPositionssender.inZone(zone);
+        }catch(Exception e){
+            exception = true;
+        }
+        Assert.assertTrue(exception);
+    }
+
+    @Test
+    public void positionssenderIstInRadius(){
+        Position position = new Position();
+        position.setLaengengrad(12.3);
+        position.setBreitengrad(34.0);
+        Positionssender positionssender = new Positionssender();
+        positionssender.setPositionssenderId("ichBinImRadius");
+        positionssender.setPosition(position);
+
+
+        boolean result = positionssender.isInnerhalbRadius(5.0,position.getLaengengrad(),position.getBreitengrad());
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void positionssenderIstNichtImRadius(){
+        Position position = new Position();
+        position.setLaengengrad(180.0);
+        position.setBreitengrad(180.0);
+        Positionssender positionssender = new Positionssender();
+        positionssender.setPositionssenderId("ichBinNichtImRadius");
+        positionssender.setPosition(position);
+
+
+        boolean result = positionssender.isInnerhalbRadius(5.0,1.0,1.0);
+        Assert.assertFalse(result);
 
     }
 }
