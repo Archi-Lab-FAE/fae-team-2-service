8a1e8a3a7dbb152fb77be41fc77089492937e840
RedAvenge <thepowerredstone@gmail.com>
2020-01-28 18:37:42 +0100

    Try fix h2-console not working in prod

--- src/main/java/de/th/koeln/archilab/fae/faeteam2service/configuration/SecurityConfig.java
+++ src/main/java/de/th/koeln/archilab/fae/faeteam2service/configuration/SecurityConfig.java
@@ -2,9 +2,11 @@ package de.th.koeln.archilab.fae.faeteam2service.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
